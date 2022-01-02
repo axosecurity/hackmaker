c843ec8a0f79c035754963a0a788b3735cb3915b
Axosolaman <70807261+axoguys@users.noreply.github.com>
2022-01-02 19:21:22 +0600

    Add files via upload

---
+++ hackmaker.sh
@@ -0,0 +1,20 @@
+#TOO MUCH SIMPLE SCRIPT FOR MAKIENG EVERY LINUX HACKING  OS 
+# CREATOR AXOSOLAMAN FROM AXOSECURITY
+
+
+sudo apt install git -y
+
+sudo apt install python -y
+
+sudo apt install python3 -y
+
+sudo mkdir tools_axosecurity
+
+cd tools_axosecurity
+
+sudo  git clone https://github.com/LionSec/katoolin.git  
+
+sudo chmod +x *
+
+wget -q -O - https://archive.kali.org/archive-key.asc | apt-key add
+
